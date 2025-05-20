846f2d7751530116beb84dca2321359e60ca46d5
yasminmartimiano <yasmin52@gmail.com>
2025-05-19 21:41:33 -0300

    feat: adição de dependências do SwaggerConfig e mudanças no bd com um pouquinho de erros aff

--- src/db/estudo_pessoas_table_name_2025-05-14_085635.sql
+++ src/db/estudo_pessoas_table_name_2025-05-14_085635.sql
@@ -7,9 +7,9 @@ CREATE DATABASE estudo_pessoas
 --
 Use estudo_pessoas
 
-DROP TABLE IF EXISTS `table_name`;
+DROP DATABASE estudo_pessoas;
 
-CREATE TABLE `table_name` (
+CREATE TABLE `Pessoas` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `create_time` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Create Time',
   `name` varchar(255) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE `table_name` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Cadastro de Pessoas';
 
 -- Inserção de dados de exemplo
-INSERT INTO table_name (name, telefone, endereco, imagem_perfil) VALUES
+INSERT INTO pessoas (name, telefone, endereco, imagem_perfil) VALUES
 ('João Silva', '(31) 91234-5678', 'Rua das Flores, 123', 'https://imagem.com/perfil.png'),
 ('Maria Oliveira', '(31) 99887-1234', 'Avenida Central, 456', 'https://imagem.com/perfil2.png');
 
@@ -29,3 +29,32 @@ INSERT INTO table_name (name, telefone, endereco, imagem_perfil) VALUES
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2025-05-14  8:56:50
+
+/*Criação da tabela de produtos*/
+CREATE TABLE Produtos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidadeEstoque INT NOT NULL,
+    descricao TEXT
+);
+
+/*criando a tabela de compras*/
+CREATE TABLE Compras (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  dataCompra DATETIME NOT NULL,
+  pessoa_id BIGINT NOT NULL,
+  FOREIGN KEY (pessoa_id) REFERENCES Pessoas(id)
+);
+
+
+/*criando a tabela itens de compra*/
+CREATE TABLE ItensCompra (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  compra_id INT NOT NULL,
+  produto_id INT NOT NULL,
+  quantidade INT NOT NULL,
+  precoUnitario DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (compra_id) REFERENCES Compras(id),
+  FOREIGN KEY (produto_id) REFERENCES Produtos(id)
+);
